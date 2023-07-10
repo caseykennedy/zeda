817adf77a6431e15dd2b2fca44e6cf6c4a2d480c
Casey K <tehcnicolour@gmail.com>
2023-07-10 12:14:51 -0700

    swcMin true

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,7 @@ const config = {
     /// Set this to false if you want production builds to abort if there's lint errors
     ignoreDuringBuilds: process.env.VERCEL_ENV === 'production',
   },
+  swcMinify: true,
 }
 
 export default config
