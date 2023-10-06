f7f48aa8ad34899f1726b853a1504a76cf0db0e5
Casey K <tehcnicolour@gmail.com>
2023-10-06 11:52:13 -0700

    health footer CTA styles

--- components/health/HealthPage.tsx
+++ components/health/HealthPage.tsx
@@ -81,8 +81,8 @@ const HealthPage = ({ leadership, partners, settings }: Props) => {
         <FooterCTAFigure
           src={CtaImgSrc}
           alt="Zeda Inc. | Health"
-          overlayColor="bg-yellow-600"
-          className="opacity-50 mix-blend-hard-light grayscale"
+          overlayColor="bg-transparent"
+          className="opacity-50 mix-blend-hard-light"
         />
       </FooterCTA>
     </Layout>
